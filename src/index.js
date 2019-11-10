e2b6b6d5a682aa4bb45bb75a89084c521aa68ae0
Preeti <singhp1992@gmail.com>
2019-11-10 16:34:25 +0100

    changing provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import store from './store'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
 
-ReactDOM.render(
+const store = createStore(rootReducer)
+
+render(
   <Provider store={store}>
-  <App />,
-  </Provider>
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
