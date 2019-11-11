60d9bca4d6afb71582f3274c107c1764f9c41d84
Preeti <singhp1992@gmail.com>
2019-11-11 13:58:17 +0100

    cleaning up card code

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -3,12 +3,19 @@ import CardHeader from './CardHeader';
 import CardBody from './CardBody';
 import '../../styling/cards.css';
 
+
 class Card extends React.Component {
     render() {
         return (
             <article className="card">
-                <CardHeader category={this.props.details.category} image={this.props.details.image} />
-                <CardBody title={this.props.details.title} text={this.props.details.text} />
+                <CardHeader 
+                    category={this.props.details.category} 
+                    image={this.props.details.image} 
+                />
+                <CardBody 
+                    title={this.props.details.title} 
+                    text={this.props.details.text} 
+                />
             </article>
         )
     }
