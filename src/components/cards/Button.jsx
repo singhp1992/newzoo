2b85159182e9f166d02057c4e40d015fc69e7538
Preeti <singhp1992@gmail.com>
2019-11-11 13:55:06 +0100

    adding game details to button

--- src/components/cards/Button.jsx
+++ src/components/cards/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../../styling/cards.css';
+
 class Button extends React.Component {
     render() {
         return (
             <button className="button button-primary">
-                <i className="fa fa-chevron-right"></i> Find out more
+                <i className="fa fa-chevron-right"></i> 
+                Game Details
             </button>
         )
     }
