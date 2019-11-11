159ba62d8c0935dfa87c546cbbb8be4487a22c9b
Preeti <singhp1992@gmail.com>
2019-11-11 11:13:46 +0100

    creating title component

---
+++ src/components/cards/Title.jsx
@@ -0,0 +1,11 @@
+class Title extends React.Component {
+    render() {
+        return <section className="app-title">
+            <div className="app-title-content">
+                <h1>Latest News</h1>
+                <p>Covering March & April 2015</p>
+                <a className="designer-link" href="https://dribbble.com/shots/1978243-Latest-News" target="_blank">Design from <i className="fa fa-dribbble"></i></a>
+            </div>
+        </section>
+    }
+}
