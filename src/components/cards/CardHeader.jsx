ee88014d87ff1cf2060f00dcb5aea592cae8da13
Preeti <singhp1992@gmail.com>
2019-11-11 13:59:55 +0100

    adding spaces to jsx expressions

--- src/components/cards/CardHeader.jsx
+++ src/components/cards/CardHeader.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import '../../styling/cards.css';
 
+
 class CardHeader extends React.Component {
     render() {
+
         const { image, category } = this.props;
         var style = {
             backgroundImage: 'url(' + image + ')',
         };
+
         return (
             <header style={style} className="card-header">
-                <h4 className="card-header--title">{category}</h4>
+                <h4 className="card-header--title">{ category }</h4>
             </header>
         )
     }
