e2e01b03b9566f2ccf8cad3531c790756cd5813b
Preeti <singhp1992@gmail.com>
2019-11-11 11:15:32 +0100

    creating button component

---
+++ src/components/cards/Button.jsx
@@ -0,0 +1,9 @@
+class Button extends React.Component {
+    render() {
+        return (
+            <button className="button button-primary">
+                <i className="fa fa-chevron-right"></i> Find out more
+      </button>
+        )
+    }
+}
