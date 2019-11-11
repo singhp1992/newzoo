b25bf9c27e29810f6860c2f4a87f64bc6caf45fb
Preeti <singhp1992@gmail.com>
2019-11-11 13:51:12 +0100

    removing date

--- src/components/cards/CardBody.jsx
+++ src/components/cards/CardBody.jsx
@@ -6,12 +6,8 @@ class CardBody extends React.Component {
     render() {
         return (
             <div className="card-body">
-                <p className="date">March 20 2015</p>
-
                 <h2>{this.props.title}</h2>
-
                 <p className="body-content">{this.props.text}</p>
-
                 <Button />
             </div>
         )
