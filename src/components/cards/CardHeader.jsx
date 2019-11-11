18d4b33ba3b4c0d3158c32bc3b8b29804ace419e
Preeti <singhp1992@gmail.com>
2019-11-11 11:15:58 +0100

    adding cardheader component

---
+++ src/components/cards/CardHeader.jsx
@@ -0,0 +1,13 @@
+class CardHeader extends React.Component {
+    render() {
+        const { image, category } = this.props;
+        var style = {
+            backgroundImage: 'url(' + image + ')',
+        };
+        return (
+            <header style={style} className="card-header">
+                <h4 className="card-header--title">{category}</h4>
+            </header>
+        )
+    }
+}
