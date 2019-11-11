1def62158a3ade1dc41bf81a9af915139b7568f7
Preeti <singhp1992@gmail.com>
2019-11-11 11:14:59 +0100

    adding temporary store

--- src/components/cards/Main.jsx
+++ src/components/cards/Main.jsx
@@ -1,3 +1,43 @@
+const PostsData = [
+    {
+        "category": "News",
+        "title": "Game 1",
+        "text": "CNN purchased Casey Neistat's Beme app for $25million.",
+        "image": "https://source.unsplash.com/user/erondu/600x400"
+    },
+    {
+        "category": "Travel",
+        "title": "Game 2",
+        "text": "Learn our tips and tricks on living a nomadic lifestyle",
+        "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
+    },
+    {
+        "category": "Development",
+        "title": "Game 3",
+        "text": "The first ever decoupled starter theme for React & the WP-API",
+        "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
+    },
+    {
+        "category": "News",
+        "title": "Game 4",
+        "text": "CNN purchased Casey Neistat's Beme app for $25million.",
+        "image": "https://source.unsplash.com/user/erondu/600x400"
+    },
+    {
+        "category": "Travel",
+        "title": "Game 5",
+        "text": "Learn our tips and tricks on living a nomadic lifestyle",
+        "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
+    },
+    {
+        "category": "Development",
+        "title": "Game 6",
+        "text": "The first ever decoupled starter theme for React & the WP-API",
+        "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
+    }
+]
+
+
 class Main extends React.Component {
     constructor() {
         super();
