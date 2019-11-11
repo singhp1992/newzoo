23b44ce780b241088f4904260312d26c07e3d570
Preeti <singhp1992@gmail.com>
2019-11-11 11:20:41 +0100

    importing main to appks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Main from './components/cards/Main'
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Welcome to the Game Center</h2>
         </div>
+        <Main />
       </div>
     );
   }
