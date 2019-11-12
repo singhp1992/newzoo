276d46430082c1e6e652f09829d00151eaff957e
Preeti <singhp1992@gmail.com>
2019-11-12 13:39:59 +0100

    updating indexjs without external store file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux'
-import store from './store'
+import { Provider } from 'react-redux';
+import GamesReducer from './reducers/games';
+import { createStore } from 'redux';
+// import reducers from './reducers'
+// import store from './store'
+
+const store = createStore(
+  GamesReducer
+)
 
 ReactDOM.render(
   <Provider store={store}>
